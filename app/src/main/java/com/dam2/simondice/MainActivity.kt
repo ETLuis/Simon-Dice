3056efcb070e6cd876c69e528b9f3381c42e5c23
ETLuis <lcampanogoldaro@gmail.com>
2022-10-24 13:27:11 +0200

    App 0.3 (visualizarColor + courutinas).

--- app/src/main/java/com/dam2/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/simondice/MainActivity.kt
@@ -10,6 +10,9 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,23 +21,37 @@ class MainActivity : AppCompatActivity() {
         Log.d("Estado", "Estoy en onCreate")
         val botonInicio:Button =  findViewById(R.id.button7)
         val botonAzul:Button =  findViewById(R.id.button8)
+        val botonAmarillo:Button =  findViewById(R.id.button9)
+        val botonRojo:Button =  findViewById(R.id.button10)
+        val botonVerde:Button =  findViewById(R.id.button11)
         val texto:TextView = findViewById(R.id.textView3)
         val random: Int = (0..3).random()
         val colores: Array<String> = arrayOf("Verde", "Amarillo", "Azul", "Rojo")
 
         botonInicio.setOnClickListener() {
             inicioPartida()
+            //visualizarSecuencia
+            botonInicio.visibility = View.INVISIBLE
         }
 
         botonAzul.setOnClickListener(){
-
+            visualizarColor()
         }
 
+        botonAmarillo.setOnClickListener(){
+            visualizarColor()
+        }
+        botonVerde.setOnClickListener(){
+            visualizarColor()
+        }
 
+        botonRojo.setOnClickListener(){
+            visualizarColor()
+        }
     }
 
 
-    fun inicioPartida(){
+     fun inicioPartida(){
         val botonAzul:Button =  findViewById(R.id.button8)
         val botonAmarillo:Button =  findViewById(R.id.button9)
         val botonRojo:Button =  findViewById(R.id.button10)
@@ -44,31 +61,56 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,"Has iniciado partida",Toast.LENGTH_SHORT).show()
 
+         GlobalScope.launch {
+             if (random == 0) {
+                 delay(3000L)
+                 botonAzul.setBackgroundColor(Color.CYAN)
+             }
+             if (random == 1) {
+                 delay(3000L)
+                 botonAmarillo.setBackgroundColor(Color.YELLOW)
+             }
+             if (random == 2) {
+                 delay(3000L)
+                 botonRojo.setBackgroundColor(Color.RED)
+
+             }
+             if (random == 3) {
+                 delay(3000L)
+                 botonVerde.setBackgroundColor(Color.GREEN)
+             }
+         }
+
+    }
+
+    fun visualizarColor() {
+        val botonAzul: Button = findViewById(R.id.button8)
+        val botonAmarillo: Button = findViewById(R.id.button9)
+        val botonRojo: Button = findViewById(R.id.button10)
+        val botonVerde: Button = findViewById(R.id.button11)
+        val random: Int = (0..3).random()
+
+        GlobalScope.launch {
             if (random == 0) {
                 botonAzul.setBackgroundColor(Color.CYAN)
+
             }
             if (random == 1) {
                 botonAmarillo.setBackgroundColor(Color.YELLOW)
             }
             if (random == 2) {
                 botonRojo.setBackgroundColor(Color.RED)
+
             }
             if (random == 3) {
                 botonVerde.setBackgroundColor(Color.GREEN)
+
             }
+        }
 
     }
-
-
-    fun botonAzul(){
-        val botonAzul:Button =  findViewById(R.id.button8)
-
-
-    }
-
-
-
-    fun visualizaColor() {
+/*
+    fun visualizaSecuencia() {
         var encendido: Job? = null
 
         encendido = GlobalScope.launch(Dispatchers.Main) {
@@ -84,7 +126,7 @@ class MainActivity : AppCompatActivity() {
                 " " + jobCuentaAtras.toString())
 
     }
-
+*/
 
 
 
