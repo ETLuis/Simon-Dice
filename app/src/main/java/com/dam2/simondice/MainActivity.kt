fa760d24710214a5adceabfcc109085ed46b35fa
ETLuis <lcampanogoldaro@gmail.com>
2022-10-24 09:35:45 +0200

    Hice el método inicioPartida().

--- app/src/main/java/com/dam2/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/simondice/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dam2.simondice
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,31 +16,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("Estado", "Estoy en onCreate")
+        val boton:Button =  findViewById(R.id.button7)
+        val texto:TextView = findViewById(R.id.textView3)
 
-        val random: Int = (0..3).random()
-        val colores: Array<String> = arrayOf("Green", "Yellow", "Blue", "Red")
-        val todosColores: ArrayList<String> = arrayListOf(colors(random))
-        val start = findViewById<Button>(R.id.button7)
-        val Array: Array<Class<Green>> = arrayOf(Green::class.java)
-
-        for(i: Int in 0..3){
-            random = (0..3).random()
-            todosColores.add(colores(colores))
+        boton.setOnClickListener() {
+            inicioPartida()
         }
 
-        start.setOnClickListener{
-            val intent = Intent(this@MainActivity, Green::class.java)
-            intent.putStringArrayListExtra("colors, allColors")
-            intent.putExtra("count", 0)
-            intent.putExtra("score", 0)
-            startActivity(intent)
-
-        }
 
     }
 
-
-
-
+    fun inicioPartida(){
+        Toast.makeText(this,"Has iniciado partida",Toast.LENGTH_SHORT).show()
+    }
 
 }
+
+
